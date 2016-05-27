a4774930362e94be9e7007203dd0da8c7f1e7390
Obi <obi.netbits@outlook.com>
2016-05-27 02:42:02 +0000

    Initial push

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var app = express();
+
+//visiting root page
+app.get("/", function(req, res){
+    res.send("Hi there, welcome to my express routing demo page");
+});
+
+//visiting 'animals' page
+app.get("/speak/:animalName", function(req, res){
+    var animalN = req.params.animalName;
+    var animalSound;
+    if(animalN === "pig"){
+        animalSound = "Oink";
+    }
+    
+    else if (animalN === "cow"){
+        animalSound = "Moo";
+    }
+    
+    else if (animalN === "dog"){
+        animalSound = "Woof Woof!";
+    }
+    
+    else{
+        animalSound = "Sorry, no sound found";
+    }
+    res.send("The " + animalN + " says " + "'" + animalSound + "'")
+});
+
+//visiting word print page
+
+//visting every other thing else
+app.get("*", function(req, res){
+    res.send("Sorry, page not found... What are you doing with your life?");
+});
+
+
+//Tell Express to listen for request(start over)
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("SERVER HAS STARTED!!!");
+});
